8fe9c6e8471db4f8d8ceb925a332d656a2f6ff0c
SanatK <sanatkasymov1997@gmail.com>
2020-08-15 15:58:20 +0600

    Add form for comment creation

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,2 +1,30 @@
 'use strict';
 
+function showModalWin() {
+
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin = document.getElementById('modal_window');
+    modalWin.hidden = false;
+    modalWin.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin.style.display = 'none';
+        return false;
+    };
+}
+
+const saveButton = document.getElementById("add-message");
+saveButton.addEventListener("click", function() {
+    const messageForm = document.getElementById("comment-form");
+    let data = new FormData(messageForm);
+    let place_id = data.get("place_id");
+
+    fetch("http://localhost:8080/createMessage", {
+        method: 'POST',
+        body: data
+    }).then(r => r.json()).then(data => {window.location.href = "http://localhost:8080/place/"+place_id});
+});
